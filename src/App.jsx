a888e109d71b024887fd0bda180dea7d12a35aa6
Pedro Silva <pedro.alsf94@gmail.com>
2023-08-23 14:00:40 -0300

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,123 +1,117 @@
-import './App.css'
-import React from 'react'
-import Display from './Components/Display.jsx'
-import Keyboard from './Components/Keyboard.jsx'
-
+import "./App.css";
+import React from "react";
+import Display from "./Components/Display.jsx";
+import Keyboard from "./Components/Keyboard.jsx";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      input: '',
+      input: "",
       result: null,
-      lastOp: ''
-    }
+      lastOp: "",
+    };
   }
   regex = /^\d*\.?\d*$/g;
   handleNumbers = (e) => {
-    if (this.state.input.match(this.regex)){
-    this.setState((prevState) => ({
-      input: prevState.input.concat(e.target.innerHTML)
-    }))
-  } else if (this.state.input === ''){
-    this.setState((prevState) => ({
-      input: prevState.input.concat(e.target.innerHTML)
-    }))
-  } else {
-    this.setState(() => ({
-      input: e.target.innerHTML
-    }))
-  }
-  }
+    if (this.state.input.match(this.regex)) {
+      this.setState((prevState) => ({
+        input: prevState.input.concat(e.target.innerHTML),
+      }));
+    } else if (this.state.input === "") {
+      this.setState((prevState) => ({
+        input: prevState.input.concat(e.target.innerHTML),
+      }));
+    } else {
+      this.setState(() => ({
+        input: e.target.innerHTML,
+      }));
+    }
+  };
 
   handleOperators = (e) => {
-    if (this.state.input===''){
-      this.setState(()=> ({
-        input: e.target.innerHTML
-      }))      
-    } else if (this.state.result===null){
-      this.handleEquals()
+    if (this.state.input === "") {
+      this.setState(() => ({
+        input: e.target.innerHTML,
+      }));
+    } else if (this.state.result === null) {
+      this.handleEquals();
       this.setState((prevState) => ({
-        result: Number(prevState.input)
-      }))
-    }else {
-      this.handleEquals()
+        result: Number(prevState.input),
+      }));
+    } else {
+      this.handleEquals();
     }
     this.setState({
       lastOp: e.target.innerHTML,
-      input: e.target.innerHTML
-    })
-    }
+      input: e.target.innerHTML,
+    });
+  };
 
   handleClear = () => {
     this.setState({
-      input: '',
+      input: "",
       result: null,
-      lastOp: ''
-    })
-  }
+      lastOp: "",
+    });
+  };
 
   handleEquals = () => {
-    if (!this.state.input.match(this.regex))return
+    if (!this.state.input.match(this.regex)) return;
     switch (this.state.lastOp) {
-      case '+':
-      this.setState((prevState) => ({
-      input: '',
-      result: (Number(prevState.input) + Number(prevState.result))
-     }))
-     break;
+      case "+":
+        this.setState((prevState) => ({
+          input: "",
+          result: Number(prevState.input) + Number(prevState.result),
+        }));
+        break;
 
-     case '-':
-      this.setState((prevState) => ({
-      input: '',
-      result: (Number(prevState.result) - Number(prevState.input))
-      }))
-      break;
+      case "-":
+        this.setState((prevState) => ({
+          input: "",
+          result: Number(prevState.result) - Number(prevState.input),
+        }));
+        break;
 
-    case 'x':
-    this.setState((prevState) => ({
-    input: '',
-    result: (Number(prevState.result) * Number(prevState.input))
-    }))
-    break;
+      case "x":
+        this.setState((prevState) => ({
+          input: "",
+          result: Number(prevState.result) * Number(prevState.input),
+        }));
+        break;
 
-    case '/':
-    this.setState((prevState) => ({
-    input: '',
-    result: (Number(prevState.result) / Number(prevState.input))
-    }))
-    break;
-    
+      case "/":
+        this.setState((prevState) => ({
+          input: "",
+          result: Number(prevState.result) / Number(prevState.input),
+        }));
+        break;
 
-     default:
-      return
+      default:
+        return;
     }
-    console.log('equals ran')
-   
-  }
+    console.log("equals ran");
+  };
 
-
-
-  render () {
+  render() {
     const { input, result, lastOp } = this.state;
-  return (
-    <>
-      <h1>Javascript Calculator</h1>
-      <div id='calculator'>
-      <Display 
-      input={input} 
-      result={result}
-      />
-      <Keyboard input={input} result={result} 
-      handleNumbers={this.handleNumbers} 
-      handleOperators={this.handleOperators} 
-      handleClear={this.handleClear}
-      handleEquals={this.handleEquals}
-      />
-      {console.log(input, result, lastOp)}
-      </div>
-    </>
-    )
+    return (
+      <>
+        <h1>Javascript Calculator</h1>
+        <div id="calculator">
+          <Display input={input} result={result} />
+          <Keyboard
+            input={input}
+            result={result}
+            handleNumbers={this.handleNumbers}
+            handleOperators={this.handleOperators}
+            handleClear={this.handleClear}
+            handleEquals={this.handleEquals}
+          />
+          {console.log(input, result, lastOp)}
+        </div>
+      </>
+    );
   }
 }
-export default App
+export default App;
--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,23 +1,19 @@
-import './Display.css'
-import PropTypes from 'prop-types';
+import "./Display.css";
+import PropTypes from "prop-types";
 
-const Display = ({ input, result}) => {
+const Display = ({ input, result }) => {
   return (
     <div id="display">
-      <div id='resultDisplay'> 
-      {result}
-      </div>
-      <div id='inputDisplay'>
-        {input}
-      </div>
-      </div>
-  )
-}
+      <div id="resultDisplay">{result}</div>
+      <div id="inputDisplay">{input}</div>
+    </div>
+  );
+};
 
 Display.propTypes = {
   input: PropTypes.string,
   display: PropTypes.number,
-  result: PropTypes.number
-}
+  result: PropTypes.number,
+};
 
-export default Display
+export default Display;
